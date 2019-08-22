83d01e214d9fd3e2bb93b5e66b9234d9ba85eb22
chrisli5 <chrisli41@gmail.com>
2019-08-22 14:59:59 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors());
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
