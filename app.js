565502f6db155b9ed32249f34b250b4bb9281878
chrisli5 <chrisli41@gmail.com>
2019-08-22 15:25:23 -0700

    completed exercise

--- app.js
+++ app.js
@@ -3,12 +3,47 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const MOVIES = require('./MOVIES');
 
 const app = express();
 
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors());
+
+app.use(function validateToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized access' })
+    }
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json('Hello Express');
+});
+
+app.get('/movie', (req, res) => {
+    const { genre, country, avg_vote } = req.query;
+    let response = MOVIES;
+
+    if(genre) {
+        response = response.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+    }
+
+    if(country) {
+        response = response.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
+    }
+
+    if(avg_vote) {
+        const vote = Number(avg_vote) || 0;
+        response = response.filter(movie => Number(movie.avg_vote) >= vote);
+    }
+
+    res.json(response);
+})
 
 const PORT = 8000;
 
